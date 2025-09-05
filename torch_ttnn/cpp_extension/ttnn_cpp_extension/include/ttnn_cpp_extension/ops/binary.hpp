f0bb7a0a8b727f43e933065f0f17bfecc4743c33
Ilia Shutov <Ilia_Shutov@epam.com>
2025-09-05 17:49:54 +0200

    In progress: draft impl of add, sub via templates

--- torch_ttnn/cpp_extension/ttnn_cpp_extension/include/ttnn_cpp_extension/ops/binary.hpp
+++ torch_ttnn/cpp_extension/ttnn_cpp_extension/include/ttnn_cpp_extension/ops/binary.hpp
@@ -6,4 +6,8 @@
 namespace tt_eager::ops::binary {
 at::Tensor& ttnn_add_out(const at::Tensor& input, const at::Tensor& other, const at::Scalar& alpha, at::Tensor& out);
 at::Tensor ttnn_add_tensor(const at::Tensor& input, const at::Tensor& other, const at::Scalar& alpha);
+
+// Added now for sub.Tensor
+at::Tensor& ttnn_sub_out(const at::Tensor& input, const at::Tensor& other, const at::Scalar& alpha, at::Tensor& out);
+at::Tensor ttnn_sub_tensor(const at::Tensor& input, const at::Tensor& other, const at::Scalar& alpha);
 }  // namespace tt_eager::ops::binary
--- torch_ttnn/cpp_extension/ttnn_cpp_extension/src/ops/binary.cpp
+++ torch_ttnn/cpp_extension/ttnn_cpp_extension/src/ops/binary.cpp
@@ -8,54 +8,28 @@
 #include "ttnn_cpp_extension/ops/creation.hpp"
 
 #include "ttnn_cpp_extension/utils/extension_utils.hpp"
+#include "ttnn_cpp_extension/utils/eager_wrap.hpp"
 
 namespace tt_eager::ops::binary {
-at::Tensor& ttnn_add_out(const at::Tensor& input, const at::Tensor& other, const at::Scalar& alpha, at::Tensor& out) {
-    LOGGING("input device: {}, other device: {}", input.device(), other.device());
-    TORCH_CHECK(input.device().type() == c10::DeviceType::PrivateUse1);
-    TORCH_CHECK(other.device().type() == c10::DeviceType::PrivateUse1);
+    // TODO: not required to write ttnn_add_tensor, ttnn_add_out - should be possible to pass these directly in torch_ttnn/cpp_extension/ttnn_cpp_extension/src/open_registration_extension.cpp
+    using add_k = tt_eager::ext::binary_kernel<ttnn::add>;
 
-    at::TtnnTensorImpl* tensor_impl = static_cast<at::TtnnTensorImpl*>(input.unsafeGetTensorImpl());
-    auto ttnn_tensor_input = tensor_impl->get_ttnn_tensor();
-    if (ttnn_tensor_input.layout() == ttnn::ROW_MAJOR_LAYOUT) {
-        ttnn_tensor_input = ttnn::to_layout(
-            ttnn_tensor_input, ttnn::TILE_LAYOUT, std::nullopt, std::nullopt, ttnn_tensor_input.device());
+    inline at::Tensor ttnn_add_tensor(const at::Tensor& a, const at::Tensor& b) {
+        return add_k::func(a, b);
     }
 
-    at::TtnnTensorImpl* other_tensor_impl = static_cast<at::TtnnTensorImpl*>(other.unsafeGetTensorImpl());
-    auto ttnn_tensor_other = other_tensor_impl->get_ttnn_tensor();
-    if (ttnn_tensor_other.layout() == ttnn::ROW_MAJOR_LAYOUT) {
-        ttnn_tensor_other = ttnn::to_layout(
-            ttnn_tensor_other, ttnn::TILE_LAYOUT, std::nullopt, std::nullopt, ttnn_tensor_other.device());
+    inline at::Tensor& ttnn_add_out(const at::Tensor& a, const at::Tensor& b, at::Tensor& out) {
+        return add_k::func_out(a, b, out);
     }
 
-    auto result = ttnn::add(ttnn_tensor_input, ttnn_tensor_other);
+    using sub_k = tt_eager::ext::binary_kernel<ttnn::subtract>;
 
-    // tensor_impl->set_ttnn_tensor(result);
-    // Get underlying TTNN tensor object from output
-    at::TtnnTensorImpl* out_tensor_impl = static_cast<at::TtnnTensorImpl*>(out.unsafeGetTensorImpl());
-    out_tensor_impl->set_sizes_and_strides_as(input);
+    inline at::Tensor ttnn_sub_tensor(const at::Tensor& a, const at::Tensor& b) {
+        return sub_k::func(a, b);
+    }
 
-    // Set output TTNN tensor to result
-    auto out_ttnn_tensor = out_tensor_impl->get_ttnn_tensor();
-    out_tensor_impl->set_ttnn_tensor(result);
-    return out;
-}
+    inline at::Tensor& ttnn_sub_out(const at::Tensor& a, const at::Tensor& b, at::Tensor& out) {
+        return sub_k::func_out(a, b, out);
+    }
 
-// Add this function to your open_registration_extension.cpp
-at::Tensor ttnn_add_tensor(const at::Tensor& input, const at::Tensor& other, const at::Scalar& alpha) {
-    LOGGING("Creating new output tensor");
-    // Create a new output tensor on the right device
-    // auto output = at::empty(input.sizes(), input.options());
-    auto output = tt_eager::ops::create::custom_empty_memory_format(
-        input.sizes(),
-        c10::optional<at::ScalarType>(input.scalar_type()),
-        c10::nullopt,  // layout
-        c10::optional<at::Device>(input.device()),
-        c10::nullopt  // pin_memory
-    );
-
-    // Call the existing out version
-    return ttnn_add_out(input, other, alpha, output);
-}
 }  // namespace tt_eager::ops::binary
