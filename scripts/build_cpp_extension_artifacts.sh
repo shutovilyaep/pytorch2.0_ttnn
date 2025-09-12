caae39634cf70403d8c47f8a69d67665be672742
Ilia Shutov <Ilia_Shutov@epam.com>
2025-09-12 10:48:47 +0000

    DEBUG: scripts, tags info

--- scripts/build_cpp_extension_artifacts.sh
+++ scripts/build_cpp_extension_artifacts.sh
@@ -1,28 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-TT_METAL_VERSION=v0.58.1
-TT_METAL_HOME=/home/epam/shutov/tt-metal/$TT_METAL_VERSION
-export TT_METAL_HOME=$TT_METAL_HOME
-
-# Prefer external tt-metal venv if present; otherwise fall back to local submodule and create venv if needed
-if [ -f "$TT_METAL_HOME/python_env/bin/activate" ]; then
-    source "$TT_METAL_HOME/python_env/bin/activate"
-    echo "Using venv from version $TT_METAL_VERSION"
-else
-    echo "WEIRD"
-    # LOCAL_TT_METAL="/home/epam/shutov/pytorch2.0_ttnn/torch_ttnn/cpp_extension/third-party/tt-metal"
-    # if [ ! -f "$LOCAL_TT_METAL/python_env/bin/activate" ]; then
-    #     echo "Local tt-metal venv not found. Creating one at $LOCAL_TT_METAL/python_env ..."
-    #     pushd "$LOCAL_TT_METAL" >/dev/null
-    #     ./create_venv.sh
-    #     popd >/dev/null
-    # fi
-    # source "$LOCAL_TT_METAL/python_env/bin/activate"
-    # export TT_METAL_HOME="$LOCAL_TT_METAL"
-fi
-
-
 CACHE_DIR=/home/epam/shutov/.cache/cpp-extension-cache
 
 # # Added pre-build steps with venv recreation for clean build
@@ -53,85 +31,23 @@ CACHE_DIR=/home/epam/shutov/.cache/cpp-extension-cache
 # sed -i '/^configure_hugepages() {/,/^}/c\configure_hugepages() {\n    echo "Skip hugepages installation"\n}' ./torch_ttnn/cpp_extension/third-party/tt-metal/install_dependencies.sh
 # ./torch_ttnn/cpp_extension/third-party/tt-metal/install_dependencies.sh
 
-# DEBUG: DONE
 echo "[4/6] Install Python dependencies"
-# python3 -m ensurepip --upgrade || true
-# python3 -m pip config set global.extra-index-url https://download.pytorch.org/whl/cpu
-# python3 -m pip install -r requirements-dev.txt
-# python3 -m pip install pytest-github-report
-# # Match CI behavior: keep pip at 21.2.4 and avoid upgrading numpy to 2.x
-# # python3 -m pip install --force-reinstall 'pip==21.2.4'
-# # python3 -m pip install 'numpy>=1.24.4,<2' setuptools wheel
-# python3 -m pip install -e .
+python3 -m ensurepip --upgrade || true
+python3 -m pip config set global.extra-index-url https://download.pytorch.org/whl/cpu
+python3 -m pip install -r requirements-dev.txt
+python3 -m pip install pytest-github-report
+# Match CI behavior: keep pip at 21.2.4 and avoid upgrading numpy to 2.x
+# python3 -m pip install --force-reinstall 'pip==21.2.4'
+# python3 -m pip install 'numpy>=1.24.4,<2' setuptools wheel
+python3 -m pip install -e .
 
-# # echo "[5/6] Sync submodules and build C++ extensions"
-# # git submodule sync
-# # git submodule update --init --recursive
-# # git submodule foreach 'git lfs fetch --all && git lfs pull'
+# echo "[5/6] Sync submodules and build C++ extensions"
+# git submodule sync
+# git submodule update --init --recursive
+# git submodule foreach 'git lfs fetch --all && git lfs pull'
 
 pushd torch_ttnn/cpp_extension >/dev/null
-# ./build_cpp_extension.sh
-# + ./build_cpp_extension.sh content
-#!/bin/bash
-
-# parse build type
-BUILD_TYPE=${1:-Release}
-echo "> Build type: $BUILD_TYPE"
-
-# Current directory
-# CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-# echo "> Current directory: $CUR_DIR"
-
-# ABI flags used when compiling torch
-# TORCH_ABI_FLAGS=$(python3 $TT_METAL_HOME/utils/get_torch_abi_flags.py)
-TORCH_ABI_FLAGS=$(python3 utils/get_torch_abi_flags.py)
-# TORCH_ABI_FLAGS=$(python3 $CUR_DIR/utils/get_torch_abi_flags.py)
-echo "> TORCH_ABI_FLAGS: $TORCH_ABI_FLAGS"
-
-# Configure ttnn
-# TODO: check if c++17 is enough
-echo "> Configuring ttnn"
-cmake -B $TT_METAL_HOME/build \
-    -G Ninja \
-    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-    -DCMAKE_INSTALL_PREFIX=$TT_METAL_HOME/build \
-    -DCMAKE_DISABLE_PRECOMPILE_HEADERS=TRUE \
-    -DENABLE_CCACHE=TRUE \
-    -DCMAKE_EXPORT_COMPILE_COMMANDS=OFF \
-    -DTT_UNITY_BUILDS=ON \
-    -DTT_ENABLE_LIGHT_METAL_TRACE=ON \
-    -DWITH_PYTHON_BINDINGS=ON \
-    -DCMAKE_TOOLCHAIN_FILE=$TT_METAL_HOME/cmake/x86_64-linux-torch-toolchain.cmake \
-    -DCMAKE_CXX_FLAGS="${TORCH_ABI_FLAGS} -std=c++20" \
-    -S $TT_METAL_HOME
-
-# cmake -B $CUR_DIR/third-party/tt-metal/build \
-#     -G Ninja \
-#     -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-#     -DCMAKE_INSTALL_PREFIX=$CUR_DIR/third-party/tt-metal/build \
-#     -DCMAKE_DISABLE_PRECOMPILE_HEADERS=TRUE \
-#     -DENABLE_CCACHE=TRUE \
-#     -DCMAKE_EXPORT_COMPILE_COMMANDS=OFF \
-#     -DTT_UNITY_BUILDS=ON \
-#     -DTT_ENABLE_LIGHT_METAL_TRACE=ON \
-#     -DWITH_PYTHON_BINDINGS=ON \
-#     -DCMAKE_TOOLCHAIN_FILE=$CUR_DIR/cmake/x86_64-linux-torch-toolchain.cmake \
-#     -DCMAKE_CXX_FLAGS="${TORCH_ABI_FLAGS} -std=c++20" \
-#     -S $CUR_DIR/third-party/tt-metal
-
-
-# Build ttnn
-echo "> Building ttnn"
-ninja -C $TT_METAL_HOME/build install
-
-pip3 install -e $TT_METAL_HOME/
-
-# export TT_METAL_HOME=$CUR_DIR/third-party/tt-metal
-echo "> TT_METAL_HOME: $TT_METAL_HOME"
-echo "> Building cpp extension"
-CMAKE_FLAGS="-DCMAKE_BUILD_TYPE=${BUILD_TYPE};-DCMAKE_C_COMPILER_LAUNCHER=ccache;-DCMAKE_CXX_COMPILER_LAUNCHER=ccache;-DCMAKE_CXX_COMPILER=g++-12;-DCMAKE_C_COMPILER=gcc-12" python3 setup.py develop
-
-# - ./build_cpp_extension.sh content
+./build_cpp_extension.sh
 popd >/dev/null
 
 echo "[6/6] Collect build artifacts into CACHE_DIR=$CACHE_DIR"
@@ -146,11 +62,8 @@ cp -r "ttnn_device_extension${PYTHON_LIB_SUFFIX}" "$CACHE_DIR"
 
 # tt-metal cache
 mkdir -p "$CACHE_DIR/tt-metal"
-cp -r $TT_METAL_HOME/build "$CACHE_DIR/tt-metal/"
-cp -r $TT_METAL_HOME/.cpmcache "$CACHE_DIR/tt-metal/"
-# mkdir -p "$CACHE_DIR/tt-metal"
-# cp -r third-party/tt-metal/build "$CACHE_DIR/tt-metal/"
-# cp -r third-party/tt-metal/.cpmcache "$CACHE_DIR/tt-metal/"
+cp -r third-party/tt-metal/build "$CACHE_DIR/tt-metal/"
+cp -r third-party/tt-metal/.cpmcache "$CACHE_DIR/tt-metal/"
 
 # ccache
 cp -r "$CCACHE_DIR" "$CACHE_DIR"
