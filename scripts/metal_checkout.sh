1b831b3d1e7cdfd668d3db8da76508f5b483e7b1
Ilia Shutov <Ilia_Shutov@epam.com>
2025-09-17 13:19:17 +0000

    [Local] DEBUG: scripts

---
+++ scripts/metal_checkout.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# I will pass as a parameter tt-metal's tag to chechout like v0.58.0-rc25
+METAL_TAG=$1
+
+pushd torch_ttnn/cpp_extension/third-party/tt-metal
+git checkout $METAL_TAG
+git submodule sync --recursive
+git submodule update --init --recursive --progress
+popd
