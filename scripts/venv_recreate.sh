82b2f518f05272f645a0f128b5d4074efa7faf3d
Ilia Shutov <Ilia_Shutov@epam.com>
2025-09-17 13:19:17 +0000

    [Local] DEBUG: scripts

---
+++ scripts/venv_recreate.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export TT_METAL_HOME=$(realpath torch_ttnn/cpp_extension/third-party/tt-metal)
+echo "> TT_METAL_HOME: $TT_METAL_HOME"
+
+# Added pre-build steps with venv recreation for clean build
+pushd torch_ttnn/cpp_extension/third-party/tt-metal >/dev/null
+./build_metal.sh
+rm -rf python_env
+./create_venv.sh
+source ./python_env/bin/activate
+popd >/dev/null
