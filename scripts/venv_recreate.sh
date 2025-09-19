5a88f3da03c64b58df3a68236de3d4cbc543b845
Ilia Shutov <Ilia_Shutov@epam.com>
2025-09-19 14:07:20 +0000

    SCRITPS+DEBUG: scripts upd: large build_cpp_extension - to use the same toolchain as tt-metal

--- scripts/venv_recreate.sh
+++ scripts/venv_recreate.sh
@@ -3,9 +3,13 @@
 export TT_METAL_HOME=$(realpath torch_ttnn/cpp_extension/third-party/tt-metal)
 echo "> TT_METAL_HOME: $TT_METAL_HOME"
 
+rm -rf torch_ttnn/cpp_extension/build
+rm -rf torch_ttnn/cpp_extension/third-party/tt-metal/build
+
 # Added pre-build steps with venv recreation for clean build
 pushd torch_ttnn/cpp_extension/third-party/tt-metal >/dev/null
 ./build_metal.sh
+# ./build_metal.sh --enable-ccache
 rm -rf python_env
 ./create_venv.sh
 source ./python_env/bin/activate
