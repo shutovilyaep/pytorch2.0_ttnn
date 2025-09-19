cc2213dc7cfdebb7446755f9da4af188ca3cf064
Ilia Shutov <Ilia_Shutov@epam.com>
2025-09-19 15:41:15 +0000

    to squash: pytorch 2.2.1

--- scripts/build_cpp_direct.sh
+++ scripts/build_cpp_direct.sh
@@ -6,7 +6,7 @@ source $TT_METAL_HOME/python_env/bin/activate
 
 # Ensure modern build tooling and required deps (torch/torchvision) in this venv BEFORE building the C++ extension
 python -m pip install --upgrade pip setuptools wheel setuptools_scm build
-python -m pip install 'torch==2.7.1+cpu' 'torchvision==0.22.1+cpu' --extra-index-url https://download.pytorch.org/whl/cpu
+python -m pip install 'torch==2.2.1+cpu' 'torchvision==0.17.1+cpu' --extra-index-url https://download.pytorch.org/whl/cpu
 
 pushd torch_ttnn/cpp_extension/
 ./build_cpp_extension.sh
