752c2c95b778397a5f8a481aa523edb9867cf97c
Ilia Shutov <Ilia_Shutov@epam.com>
2025-09-19 15:21:27 +0000

    tt-metal API change: build fix, test script works

--- torch_ttnn/cpp_extension/ttnn_cpp_extension/src/core/TtnnCustomAllocator.cpp
+++ torch_ttnn/cpp_extension/ttnn_cpp_extension/src/core/TtnnCustomAllocator.cpp
@@ -3,7 +3,7 @@
 #include "ttnn_cpp_extension/utils/extension_utils.hpp"
 #include <cstring>
 
-c10::DataPtr TtnnCustomAllocator::allocate(size_t nbytes) const {
+c10::DataPtr TtnnCustomAllocator::allocate(size_t nbytes) {
     LOGGING("");
     // Do not allocate any cpu space here
     void* data = nullptr;
@@ -16,6 +16,10 @@ void TtnnCustomAllocator::ReportAndDelete(void* ptr) {
 }
 
 c10::DeleterFnPtr TtnnCustomAllocator::raw_deleter() const { return &ReportAndDelete; }
+
+void TtnnCustomAllocator::copy_data(void* dest, const void* src, std::size_t count) const {
+    std::memcpy(dest, src, count);
+}
 
 TtnnCustomAllocator& get_ttnn_custom_allocator() {
     static TtnnCustomAllocator allocator;
