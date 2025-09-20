a42b6c3712df7760079be620370fce9f039eaae6
Ilia Shutov <shutovilya@gmail.com>
2025-09-20 12:34:34 +0200

    Build fix attempt +
    sudo apt-get update && sudo apt-get install -y cargo rustc pkg-config protobuf-compiler

--- scripts/venv_recreate.sh
+++ scripts/venv_recreate.sh
@@ -3,6 +3,9 @@
 # Added pre-build steps with venv recreation for clean build
 pushd torch_ttnn/cpp_extension/third-party/tt-metal >/dev/null
 unset Boost_DIR BOOST_ROOT
+export CARGO_HOME="${HOME}/.cargo"
+export RUSTUP_HOME="${HOME}/.rustup"
+mkdir -p "${CARGO_HOME}" "${RUSTUP_HOME}"
 rm -rf .cpmcache build build_Release build_Debug ~/.cache/tt-metal-cache /tmp/tt-metal-cache
 ./build_metal.sh
 rm -rf python_env
