9b1e23cb1028402a70f72596859069032f960a5b
Ilia Shutov <shutovilya@gmail.com>
2025-09-20 12:11:05 +0200

    venv_recreate upd

--- scripts/venv_recreate.sh
+++ scripts/venv_recreate.sh
@@ -2,8 +2,11 @@
 
 # Added pre-build steps with venv recreation for clean build
 pushd torch_ttnn/cpp_extension/third-party/tt-metal >/dev/null
+unset Boost_DIR BOOST_ROOT
+rm -rf .cpmcache build build_Release build_Debug ~/.cache/tt-metal-cache /tmp/tt-metal-cache
 ./build_metal.sh
 rm -rf python_env
+export PYTHON_ENV_DIR="$(pwd)/python_env"
 ./create_venv.sh
 source ./python_env/bin/activate
 popd >/dev/null
