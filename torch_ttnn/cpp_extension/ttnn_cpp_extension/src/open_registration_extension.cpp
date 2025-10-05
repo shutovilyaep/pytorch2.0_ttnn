deef4b31dc0ae9627e6b942a69fa9182783e8afb
Ilia Shutov <shutovilya@gmail.com>
2025-10-05 12:09:05 +0000

    ref, simplified

--- torch_ttnn/cpp_extension/ttnn_cpp_extension/src/open_registration_extension.cpp
+++ torch_ttnn/cpp_extension/ttnn_cpp_extension/src/open_registration_extension.cpp
@@ -25,7 +25,6 @@
 #include <ttnn/operations/moreh/moreh_dot/moreh_dot.hpp>
 #include <ttnn/operations/matmul/matmul.hpp>
 #include <string>
-#include <initializer_list>
 
 // Register custom allocator. Only used to create dummy Torch tensor object.
 REGISTER_ALLOCATOR(c10::DeviceType::PrivateUse1, &get_ttnn_custom_allocator());
@@ -35,35 +34,26 @@ namespace {
 
 // Helper templates to reduce registration boilerplate for unary ops
 template <template<auto> class Wrapper, auto Op>
-static inline void register_unary_triplet(torch::Library& m, std::initializer_list<const char*> names) {
-    for (const char* n : names) {
-        const std::string base(n);
-        const std::string out = base + ".out";
-        const std::string inplace = base + "_";
-        m.impl(base.c_str(), TORCH_FN(Wrapper<Op>::invoke));
-        m.impl(out.c_str(), TORCH_FN(Wrapper<Op>::invoke_into));
-        m.impl(inplace.c_str(), TORCH_FN(Wrapper<Op>::invoke_inplace));
-    }
+static inline void register_unary_base_out_inplace(torch::Library& m, const std::string& base) {
+    const std::string out = base + ".out";
+    const std::string inplace = base + "_";
+    m.impl(base.c_str(), TORCH_FN(Wrapper<Op>::invoke));
+    m.impl(out.c_str(), TORCH_FN(Wrapper<Op>::invoke_into));
+    m.impl(inplace.c_str(), TORCH_FN(Wrapper<Op>::invoke_inplace));
 }
 
 template <template<auto> class Wrapper, auto Op>
-static inline void register_unary_base_out(torch::Library& m, std::initializer_list<const char*> names) {
-    for (const char* n : names) {
-        const std::string base(n);
-        const std::string out = base + ".out";
-        m.impl(base.c_str(), TORCH_FN(Wrapper<Op>::invoke));
-        m.impl(out.c_str(), TORCH_FN(Wrapper<Op>::invoke_into));
-    }
+static inline void register_unary_base_out(torch::Library& m, const std::string& base) {
+    const std::string out = base + ".out";
+    m.impl(base.c_str(), TORCH_FN(Wrapper<Op>::invoke));
+    m.impl(out.c_str(), TORCH_FN(Wrapper<Op>::invoke_into));
 }
 
 template <template<auto> class Wrapper, auto Op>
-static inline void register_unary_base_inplace(torch::Library& m, std::initializer_list<const char*> names) {
-    for (const char* n : names) {
-        const std::string base(n);
-        const std::string inplace = base + "_";
-        m.impl(base.c_str(), TORCH_FN(Wrapper<Op>::invoke));
-        m.impl(inplace.c_str(), TORCH_FN(Wrapper<Op>::invoke_inplace));
-    }
+static inline void register_unary_base_inplace(torch::Library& m, const std::string& base) {
+    const std::string inplace = base + "_";
+    m.impl(base.c_str(), TORCH_FN(Wrapper<Op>::invoke));
+    m.impl(inplace.c_str(), TORCH_FN(Wrapper<Op>::invoke_inplace));
 }
 
 static inline void register_core_creation_and_copy(torch::Library& m) {
@@ -87,57 +77,59 @@ static inline void register_unary_ops(torch::Library& m) {
     // =========================
     // Unary ops
     // =========================
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::abs>(m, {"abs", "absolute"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::neg>(m, {"neg"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::reciprocal>(m, {"reciprocal"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::sqrt>(m, {"sqrt"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::rsqrt>(m, {"rsqrt"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::square>(m, {"square"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::sin>(m, {"sin"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::cos>(m, {"cos"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::tan>(m, {"tan"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::sinh>(m, {"sinh"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::cosh>(m, {"cosh"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::tanh>(m, {"tanh"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::floor>(m, {"floor"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::ceil>(m, {"ceil"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::trunc>(m, {"trunc"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::frac>(m, {"frac"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::bitwise_not>(m, {"bitwise_not"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::logical_not>(m, {"logical_not"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::sign>(m, {"sign"});
-    register_unary_base_out<tt_eager::ext::unary_tensor, ttnn::signbit>(m, {"signbit"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::i0>(m, {"i0"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::erf>(m, {"erf"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::erfc>(m, {"erfc"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::erfinv>(m, {"erfinv"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::exp>(m, {"exp"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::log>(m, {"log"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::log10>(m, {"log10"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::log2>(m, {"log2"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::log1p>(m, {"log1p"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::acos>(m, {"acos"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::acosh>(m, {"acosh", "arccosh"});
-    register_unary_triplet<tt_eager::ext::complex_unary_from_real, ttnn::angle>(m, {"angle"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::asin>(m, {"asin"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::asinh>(m, {"asinh"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::atan>(m, {"atan"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::atanh>(m, {"atanh"});
-    register_unary_triplet<tt_eager::ext::complex_unary_from_real, ttnn::conj>(m, {"conj"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::deg2rad>(m, {"deg2rad"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::digamma>(m, {"digamma"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::expm1>(m, {"expm1"});
-    register_unary_base_out<tt_eager::ext::unary_tensor, ttnn::isfinite>(m, {"isfinite"});
-    register_unary_base_out<tt_eager::ext::unary_tensor, ttnn::isinf>(m, {"isinf"});
-    register_unary_base_out<tt_eager::ext::unary_tensor, ttnn::isnan>(m, {"isnan"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::lgamma>(m, {"lgamma"});
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::rad2deg>(m, {"rad2deg"});
-    register_unary_base_inplace<tt_eager::ext::unary_tensor, ttnn::relu>(m, {"relu"});
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::abs>(m, "abs");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::abs>(m, "absolute");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::neg>(m, "neg");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::reciprocal>(m, "reciprocal");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::sqrt>(m, "sqrt");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::rsqrt>(m, "rsqrt");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::square>(m, "square");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::sin>(m, "sin");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::cos>(m, "cos");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::tan>(m, "tan");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::sinh>(m, "sinh");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::cosh>(m, "cosh");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::tanh>(m, "tanh");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::floor>(m, "floor");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::ceil>(m, "ceil");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::trunc>(m, "trunc");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::frac>(m, "frac");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::bitwise_not>(m, "bitwise_not");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::logical_not>(m, "logical_not");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::sign>(m, "sign");
+    register_unary_base_out<tt_eager::ext::unary_tensor, ttnn::signbit>(m, "signbit");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::i0>(m, "i0");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::erf>(m, "erf");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::erfc>(m, "erfc");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::erfinv>(m, "erfinv");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::exp>(m, "exp");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::log>(m, "log");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::log10>(m, "log10");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::log2>(m, "log2");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::log1p>(m, "log1p");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::acos>(m, "acos");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::acosh>(m, "acosh");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::acosh>(m, "arccosh");
+    register_unary_base_out_inplace<tt_eager::ext::complex_unary_from_real, ttnn::angle>(m, "angle");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::asin>(m, "asin");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::asinh>(m, "asinh");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::atan>(m, "atan");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::atanh>(m, "atanh");
+    register_unary_base_out_inplace<tt_eager::ext::complex_unary_from_real, ttnn::conj>(m, "conj");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::deg2rad>(m, "deg2rad");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::digamma>(m, "digamma");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::expm1>(m, "expm1");
+    register_unary_base_out<tt_eager::ext::unary_tensor, ttnn::isfinite>(m, "isfinite");
+    register_unary_base_out<tt_eager::ext::unary_tensor, ttnn::isinf>(m, "isinf");
+    register_unary_base_out<tt_eager::ext::unary_tensor, ttnn::isnan>(m, "isnan");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::lgamma>(m, "lgamma");
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::rad2deg>(m, "rad2deg");
+    register_unary_base_inplace<tt_eager::ext::unary_tensor, ttnn::relu>(m, "relu");
     // round family: special wrapper with optional int parameter
-    register_unary_triplet<tt_eager::ext::unary_tensor_opt_int_none, ttnn::round>(m, {"round"});
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor_opt_int_none, ttnn::round>(m, "round");
     m.impl("round.decimals", TORCH_FN(tt_eager::ext::unary_tensor_opt_int<ttnn::round>::invoke_decimals));
     m.impl("round.decimals_out", TORCH_FN(tt_eager::ext::unary_tensor_opt_int<ttnn::round>::invoke_decimals_into));
-    register_unary_triplet<tt_eager::ext::unary_tensor, ttnn::sigmoid>(m, {"sigmoid"});
+    register_unary_base_out_inplace<tt_eager::ext::unary_tensor, ttnn::sigmoid>(m, "sigmoid");
     
 }
 
