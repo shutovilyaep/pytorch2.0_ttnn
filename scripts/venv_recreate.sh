07ae4531e25faf4af7dd566692e387156533a4bb
Ilia Shutov <Ilia_Shutov@epam.com>
2025-10-15 14:54:45 +0000

    script upd

--- scripts/venv_recreate.sh
+++ scripts/venv_recreate.sh
@@ -7,12 +7,15 @@ echo "> TT_METAL_HOME: $TT_METAL_HOME"
 pushd torch_ttnn/cpp_extension/third-party/tt-metal >/dev/null
 # ./build_metal.sh 
 # ./build_metal.sh --build-type RelWithDebInfo --build-tests --export-compile-commands --without-distributed --clean
-rm -rf build
-rm -rf .cpmcache
 
+# rm -rf build
+# rm -rf .cpmcache
 # ./build_metal.sh --build-type RelWithDebInfo --build-tests --export-compile-commands --clean
+
 ./build_metal.sh --build-type RelWithDebInfo --build-tests --export-compile-commands
+
 rm -rf python_env
 ./create_venv.sh
 source ./python_env/bin/activate
+
 popd >/dev/null
